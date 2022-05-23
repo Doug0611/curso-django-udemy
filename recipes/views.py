d4bda2c5ecd73de617e904ca3a5894db789d7359
Douglas Sales <douglas.sales0611@gmail.com>
2022-05-23 20:54:54 -0300

    global template inheritance creation

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html')
 
     
 
